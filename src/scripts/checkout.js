e08f491db7685f0f7f0970b315dc4debde7f4b1a
hoanghai1212 <hoanghai129612@gmail.com>
2020-07-10 16:37:57 +0700

    done desktop checkout

--- src/scripts/checkout.js
+++ src/scripts/checkout.js
@@ -1,46 +1,64 @@
 import { validateEmail, validatePhone } from "./vendor/utility.js"
 
-
-let labelList = document.querySelector(`#checkOutForm`).querySelectorAll(`input`);
-
-labelList.forEach(el =>
-{
-    el.addEventListener("focusin", () =>
-    {
-        el.parentNode.querySelector(`label`).classList.toggle("onfocus");
-    });
-
-    el.addEventListener("focusout", () =>
-    {
-        el.parentNode.querySelector(`label`).classList.toggle("onfocus");
-    });
-})
-
 const dataValidation = (id, callback) =>
 {
     let inputElemt = document.querySelector(id);
     let labelElemt = inputElemt.parentNode.querySelector(`label`);
     let error = inputElemt.parentNode.querySelector(`.checkout__error`);
+    let clearBtn = inputElemt.parentNode.parentNode.querySelector(`.clearBtn`);
+
+    const inputAnimation = () =>
+    {
+        inputElemt.addEventListener("focusin", () =>
+        {
+            labelElemt.classList.add("onfocus");
+        });
+
+        inputElemt.addEventListener("focusout", () =>
+        {
+            labelElemt.classList.remove("onfocus");
+
+            if (inputElemt.value !== "")
+            {
+                labelElemt.classList.add("filled");
+            } else
+            {
+                console.log("a");
+                labelElemt.classList.remove("filled");
+            }
+        });
+    };
+    inputAnimation();
 
     inputElemt.addEventListener("keyup", () =>
     {
         if (inputElemt.value !== "")
         {
+            clearBtn.style.display = "block";
             if (callback(inputElemt.value) === true)
             {
-                (error.style.display = "none");
+                error.style.display = "none";
             } else
             {
-                (error.style.display = "block");
-                (error.innerHTML = `${labelElemt.innerHTML + ' ' + callback(inputElemt.value)}`);
-
+                error.style.display = "block";
+                error.innerHTML = `${labelElemt.innerHTML + ' ' + callback(inputElemt.value)}`;
             }
         } else
         {
-            (error.style.display = "block");
-            (error.innerHTML = `${labelElemt.innerHTML} không được bỏ trống`);
+            clearBtn.style.display = "none";
+            error.style.display = "block";
+            error.innerHTML = `${labelElemt.innerHTML} không được bỏ trống`;
         }
     }, false);
+
+    clearBtn.addEventListener("click", () =>
+    {
+        clearBtn.style.display = "none";
+        inputElemt.value = "";
+        labelElemt.classList.remove("filled");
+        error.style.display = "block";
+        error.innerHTML = `${labelElemt.innerHTML} không được bỏ trống`;
+    })
 }
 
 dataValidation(`#userPhone`, validatePhone);
